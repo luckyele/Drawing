4cfb2acf83c493c5906a60d6c478261504c36387
Yung <541913141@qq.com>
2017-09-23 23:33:29 +0800

    9.23

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import math
+import random
 from matplotlib.pyplot import *
 
 class Dot:
@@ -6,98 +7,56 @@ class Dot:
         self.x = x
         self.y = y
 
-def getTwoDotDistance(dot1, dot2):
-    """
-    get the distance that between two dots.
-    it returns of  sqrt ((x2-x1)**2 + (y2 - y1)**2)
-    """
+def distanceOf2Dot(dot1, dot2):
     return ((dot2.x - dot1.x)**2 + (dot2.y - dot1.y)**2) ** 0.5
 
-def getMiddleOfLineSegment(dot1, dot2):
-    """
-    get the middle values of line segment.
-    it returns (x, y)
-    """
-    return (dot2.x + dot1.x) * 0.5, (dot2.y + dot1.y) * 0.5
+def get13dot(dot1, dot2):
+    return Dot(dot2.x / 3 + 2*dot1.x / 3, dot2.y / 3 + 2*dot1.y / 3)
 
-def getSlope(dot1, dot2):
-    """
-    get the slope of line (dot1,dot2)
-    """
+def get23dot(dot1, dot2):
+    return Dot(2*dot2.x / 3 + dot1.x / 3, 2*dot2.y / 3 + dot1.y / 3)
+
+def getSlopeOfLineSegment(dot1, dot2):
     return (dot2.y - dot1.y) / (dot2.x - dot1.x)
 
-def getIntercept(dot1, k):
-    """
-    get the intercept of line (dot1,dot2)
-    """
-    return dot1.y - k * dot1.x
-
-def getSlopeAndIntercept(dot1, dot2):
-    """
-    get the slope and intercept of line that (dot1,dot2)
-    """
-    k = getSlope(dot1, dot2)
-    return k, getIntercept(dot1, k)
-
-def getXY1(dot1, dot2):
-    """
-    get the one-third of line segment (dot1,dot2)
-    """
-    k = getSlope(dot1, dot2)
-    b = getIntercept(dot1, k)
-    c = k / b
-    _a = Dot(0, b)
-    _b = Dot(c, 0)
-    _c = getTwoDotDistance(_a, _b)
-    _d = getTwoDotDistance(dot1, dot2) / 3
-
-    return (_d * c) / _c + dot1.x, (_d * b ) / _c + dot1.y
-
-def getXY2(dot1, dot2):
-    """
-    get the two-third of line segment (dot1,dot2)
-    """
-    k = getSlope(dot1, dot2)
-    b = getIntercept(dot1, k)
-    c =  k / b 
-    _a = Dot(0, b)
-    _b = Dot(c, 0)
-    _c = getTwoDotDistance(_a, _b)
-    _d = getTwoDotDistance(dot1, dot2) / 3
-
-    return 2*(_d * c) / _c + dot1.x, 2*(_d * b ) / _c + dot1.y
-    
-def getXY3(dot1, dot2):
-    A = dot1
-    B = dot2
+def get33dot(dot1, dot2):
     C = Dot()
+    L = distanceOf2Dot(dot1, dot2)
+    k = getSlopeOfLineSegment(dot1, dot2)
+    cosa = 1/((k**2+1)**0.5)
+    sina = k/((k**2+1)**0.5)
+    cos60 = math.cos(math.radians(60))
+    sin60 = math.sin(math.radians(60))
+    C.x = dot1.x + (cosa * cos60 - sina * sin60) * L
+    C.y = dot1.y + (sina * cos60 + cosa * sin60) * L
+    return C
 
-    R = getTwoDotDistance(A,B)
-    C.x = dot1.x + R * math.cos(math.radians(90))
-    C.y = dot1.y + R * math.sin(math.radians(90))
-    return C.x, C.y
+def one1(dot1, dot2):
+    dotArray = []
+    m = get13dot(dot1, dot2)
+    n = get23dot(dot1, dot2)
+    c = get33dot(m, n)
+    dotArray.append(dot1)
+    dotArray.append(m)
+    dotArray.append(c)
+    dotArray.append(n)
+    dotArray.append(dot2)
+    return dotArray
 
-def one(dot1, dot2):
-
-    a = dot1
-    b = dot2
-    twofloatnumber = getXY1(a, b)
-    m = Dot(twofloatnumber[0], twofloatnumber[1])
-    twofloatnumber = getXY2(a, b)
-    n = Dot(twofloatnumber[0], twofloatnumber[1])
-    tmp = getXY3(m, n)
-    c = Dot(tmp[0], tmp[1])
-    '''paint the view
-    '''
-    plot([a.x, m.x], [a.y, m.y])
-    plot([m.x, c.x], [m.y, c.y])
-    plot([n.x, c.x], [n.y, c.y])
-    plot([n.x, b.x], [n.y, b.y])
+def drawDot(dA):
+    for i in range(len(dA) - 1):
+        plot([dA[i].x, dA[i+1].x], [dA[i].y, dA[i+1].y])
     show()
 
+def printArray(dA):
+    for i in range(len(dA)):
+        print i, dA[i].x, dA[i].y
+
 if __name__ == "__main__":
-    a = Dot(2.0, 3.0)
-    b = Dot(3.0, 4.0)
-    tmp = getXY1(a, b)
-    c = Dot(tmp[0], tmp[1])
-    one(a, b)
+    f = random.randrange(0.0, 10.0)
+    print f
+    a = Dot(float(f), float(f+1))
+    b = Dot(6.0, 5.0)
+    A = one1(a, b)
+    printArray(A)
+    drawDot(A)
